63053ee682a4da31c69cd4366aee450564553aea
mladenov1990@gmail.com <mladenov1990@gmail.com>
2026-05-11 13:57:13 +0300

    added comments

--- linux-scripts/manual_curl_test.sh
+++ linux-scripts/manual_curl_test.sh
@@ -10,8 +10,10 @@ curl --cert <cert path> \
      -d ""
 
 
-# --noproxy '*' # Ignore proxy settings (useful for internal hosts)
-# -i #Include response headers in output
-# -v -w "\nHTTP_CODE:%{http_code}" # Print HTTP status code after response
+# --noproxy '*' #Ignore proxy settings (useful for internal hosts)
+# -i #Include response headers in output (server name, cookies, date of the document, HTTP version, etc..)
+# -v -w "\nHTTP_CODE:%{http_code}" #Print HTTP status code after response
+# --connect-timeout 5 --max-time 10 #1st is only connection phase, 2nd is entire request lifecycle
+# -I #HEAD request (headers only, does not download response body)
 
 
