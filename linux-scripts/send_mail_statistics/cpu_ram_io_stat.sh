dad559d1d426ce07117c28656ba0bdddb31cb356
mladenov1990@gmail.com <mladenov1990@gmail.com>
2026-06-12 12:27:20 +0300

    added some logging

--- linux-scripts/send_mail_statistics/cpu_ram_io_stat.sh
+++ linux-scripts/send_mail_statistics/cpu_ram_io_stat.sh
@@ -4,10 +4,13 @@ cpu_num=$(nproc)
 
 #To generate load for each core "yes > /dev/null &"
 
+log_path="/tmp/cpu_io_ram_stat.log"
+
 load_5_min_alerts=0
 load_15_min_alerts=0
 idle_alerts=0
 wa_alerts=0
+date_now=$(date +"%Y-%m-%d %H:%M:%S")
 
 #Gather statistics for load average every 1 minute for 18 minutes
 for value in {1..18}; do
@@ -49,12 +52,33 @@ for value in {1..18}; do
 	done
 
 	#echo -e "\nidle_alerts: $idle_alerts\nwa_alerts: $wa_alerts\nCounter_load_15: $load_15_min_alerts\nCounter_load_5: $load_5_min_alerts\n"
+	
+	#Write some logging	
+	date_for_log=$(date +"%Y-%m-%d %H:%M:%S")
+	text_for_log=$(cat <<EOF
+===
+$date_for_log
+1. load
+load_5_min: $load_5
+load_15_min: $load_15
+
+2. idle
+$idle_array
+
+3. wa
+$wa_array
+===
+
+EOF
+)
+
+	echo "$text_for_log" >> "$log_path"
+	
 	sleep 60
 done
 
 name=$(hostname)
 current_dir="$(dirname "$0")"
-date_now=$(date +"%Y-%m-%d %H:%M:%S")
 
 #Send an e-mail notification if a threshold is exceeded
 if [ $load_15_min_alerts -ge 1 ]; then
